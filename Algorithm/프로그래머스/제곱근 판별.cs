89f06fa716e135f68178d0b638ba57ec3735b00d
GM-Gwak <115050658+GM-Gwak@users.noreply.github.com>
2022-10-28 17:28:42 +0900

    Add files via upload

---
+++ Algorithm/프로그래머스/제곱근 판별.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = 121;
+            long answer = 0;
+
+            double num = (Math.Sqrt(n));
+            if(num%1 == 0)
+            {
+                answer = (long)((num + 1) * (num + 1));
+            }
+            else
+            {
+                answer = -1;
+            }
+            Console.WriteLine(answer);
+
+
+        }
+    }
+}
