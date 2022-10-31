1393dfc636b0c65391fb311ee62d075636ab53ab
GM-Gwak <115050658+GM-Gwak@users.noreply.github.com>
2022-10-31 11:30:21 +0900

    Add files via upload

---
+++ Thread/Thread.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadTes003
+{
+    internal class Program
+    {
+        static void threadFunc()
+        {
+            Console.WriteLine("5초 후에 프로그램 종료");
+            Thread.Sleep(10000);
+            Console.WriteLine("스레드 종료");
+        }
+        static void Main(string[] args)
+        {
+            
+            Thread t = new Thread(threadFunc);
+            //t.IsBackground = true;
+            t.Start();
+            t.Join();
+            
+            Console.WriteLine("메인 스레드 종료");
+        }
+    }
+}
