35fadedf8e04ba169606e12d122d7bc5ec7dc36f
GM-Gwak <115050658+GM-Gwak@users.noreply.github.com>
2022-11-01 10:05:01 +0900

    Add files via upload

---
+++ Algorithm/프로그래머스/약수의 개수와 덧셈.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int left = 24;
+            int right = 27;
+            int cnt = 0;
+            int answer = 0;
+            for(int i = left; i <= right; i++)
+            {
+                for(int j = 1; j <= left; j++)
+                {
+                    if (left % j == 0)
+                    {
+                        cnt++;
+                    }
+                    
+                }
+                if(cnt%2 == 0)
+                {
+                    answer += left;
+                    cnt = 0;
+                    left++;
+                }
+                else
+                {
+                    answer -= left;
+                    cnt = 0;
+                    left++;
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
